03c6ea07f33e8f456bdcf762494b0589a2c16916
Ifeoluwa  Akinremi-Wade <ifeoluwa.wade@gmail.com>
2020-04-14 00:47:18 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -5,7 +5,9 @@ puts "Hi, my name is #{name}."
 end
 
 introduction("Danny")
+
 # The introduction_with_language_method
+
 def introduction(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
